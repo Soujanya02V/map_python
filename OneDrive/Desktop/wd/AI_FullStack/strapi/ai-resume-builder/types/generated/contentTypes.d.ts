38c38522351e6e8c4451573136c6182783a8dbb9
Tuvi11 <soujanyabailawad@gmail.com>
2025-12-15 11:23:02 +0530

    fetching from strapi

--- OneDrive/Desktop/wd/AI_FullStack/strapi/ai-resume-builder/types/generated/contentTypes.d.ts
+++ OneDrive/Desktop/wd/AI_FullStack/strapi/ai-resume-builder/types/generated/contentTypes.d.ts
@@ -388,7 +388,9 @@ export interface ApiUserResumeUserResume extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    education: Schema.Attribute.Component<'education.education', true>;
     email: Schema.Attribute.String;
+    experience: Schema.Attribute.Component<'experience.experience', true>;
     firstName: Schema.Attribute.String;
     jobTitle: Schema.Attribute.String;
     lastName: Schema.Attribute.String;
@@ -401,6 +403,7 @@ export interface ApiUserResumeUserResume extends Struct.CollectionTypeSchema {
     phone: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     resumeId: Schema.Attribute.String;
+    skills: Schema.Attribute.Component<'skills.skills', true>;
     summery: Schema.Attribute.Text;
     title: Schema.Attribute.String & Schema.Attribute.Required;
     updatedAt: Schema.Attribute.DateTime;
